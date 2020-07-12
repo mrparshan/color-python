d4144f6d98d628e55bd041b5a73321a3e3690a7d
Parshan Mollashahi <68120556+mrparshan@users.noreply.github.com>
2020-07-12 16:37:00 +0430

    Add files via upload

---
+++ COLOR.py
@@ -0,0 +1,26 @@
+class color:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
+    RED2 = "\033[1;31m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    GREEN2 = "\033[0;32m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    REVERSE = "\033[;7m"
+
+green = color.GREEN
+green2 = color.GREEN2
+red = color.RED
+red2 = color.RED2
+white = color.WHITE
+blue = color.BLUE
+cyan = color.CYAN
+reset_color = color.RESET
+bold_text = color.BOLD
+reverse = color.REVERSE
+print(f"{green} GITHUB: ")
+print(f"{blue} MRPARSHAN")
+print(f"{red} INSTAGRAM:")
+print(f"{green} MR_PARSHAN")
